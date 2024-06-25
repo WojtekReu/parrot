91ce82814e1db4f93b0a8a4c1880fcd287ada239
Wojciech Zając <wojtek@reu.pl>
2024-06-25 09:17:00 +0200

    feature: read alembic config from wing.config.settings

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ import sqlmodel
 
 from alembic import context
 
+from wing.config import settings
 from wing.models.book import Book
 from wing.models.flashcard import Flashcard
 from wing.models.sentence import Sentence
@@ -20,6 +21,9 @@ from wing.models.flashcard_word import FlashcardWord
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# get database configuration
+config.set_section_option('alembic', 'sqlalchemy.url', str(settings.ASYNC_POSTGRES_URI))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
